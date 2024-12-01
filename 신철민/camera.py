f50f53af1f9fcec9af706621fe866cecf9dda89c
Cheolmin Shin <chm3001@gachon.ac.kr>
2024-12-01 20:50:07 +0900

    web cam connect

---
+++ 신철민/camera.py
@@ -0,0 +1,20 @@
+import cv2
+
+# Open the device at the ID 0
+cap = cv2.VideoCapture(0)
+# Check whether user selected camera is opened successfully.
+if not (cap.isOpened()):
+    print("Could not open video device")
+
+
+while(True):
+# Capture frame-by-frame
+    ret, frame = cap.read()
+    
+    cv2.imshow('bin',frame)
+    if cv2.waitKey(100) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
