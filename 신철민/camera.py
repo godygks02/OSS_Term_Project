c4c56584e5325926d03a2921e4308181f3d05e09
Cheolmin Shin <chm3001@gachon.ac.kr>
2024-12-01 21:10:45 +0900

    web cam detects face

--- 신철민/camera.py
+++ 신철민/camera.py
@@ -6,11 +6,10 @@ cap = cv2.VideoCapture(0)
 if not (cap.isOpened()):
     print("Could not open video device")
 
-
 while(True):
 # Capture frame-by-frame
     ret, frame = cap.read()
-    
+
     cv2.imshow('bin',frame)
     if cv2.waitKey(100) & 0xFF == ord('q'):
         break
