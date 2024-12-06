7facb5f1ceb633a5ec430e6f660c8b4d813c169d
Cheolmin Shin <chm3001@gachon.ac.kr>
2024-12-06 22:24:44 +0900

    detect face

--- 신철민/camera.py
+++ 신철민/camera.py
@@ -1,19 +1,25 @@
 import cv2
 
+# face detect 
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
 # Open the device at the ID 0
 cap = cv2.VideoCapture(0)
 # Check whether user selected camera is opened successfully.
 if not (cap.isOpened()):
     print("Could not open video device")
-
-while(True):
-# Capture frame-by-frame
+    
+while True:
     ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)  # 얼굴 영역 표시
 
-    cv2.imshow('bin',frame)
-    if cv2.waitKey(100) & 0xFF == ord('q'):
+    cv2.imshow('Face Detection', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
